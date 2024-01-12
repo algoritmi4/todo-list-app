83761bd27c8283858356fb7aaa048b7ba6ac54c6
Ivan <ButorinLP@yandex.ru>
2024-01-12 20:56:58 +0300

    fixes/deploy

---
+++ index.js
@@ -0,0 +1,10 @@
+import { create, router as _router, defaults } from "json-server";
+const server = create();
+const router = _router("db.json");
+const middlewares = defaults();
+const port = 8080;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
